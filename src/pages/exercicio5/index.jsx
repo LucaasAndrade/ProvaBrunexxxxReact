4688aa3752789408da2bd629140fc03c746186f8
LucaasAndrade <lucasmarcelodeandradesilva@gmail.com>
2022-10-18 07:28:17 -0300

    Restantes 5, 6, 8, 9, 10, 11

--- src/pages/exercicio5/index.jsx
+++ src/pages/exercicio5/index.jsx
@@ -1,4 +1,4 @@
-import './index.css';
+import './index.scss';
 
 
 export default function index() {
@@ -8,7 +8,14 @@ export default function index() {
         <main className='exercicio5'>
             <div>
                 <h2> Exercicio 5 </h2>
+
+                <h5> Implementar um programa em React Js que calcule quantas paradas para abastecimeneto são necessárias para realizar uma viagem, a partir da capacidade do tanque de combustível (em litros), do consumo do veículo (KM por litro) e  da distância da viagem (km).</h5>
+
+                <p> * Você deve partir do principio que o tanque está vazio </p>
+                <p> * Os valores de entrada devem ser entendidos com decimais </p>
             </div>
+
+            
         </main>
     )
 }
