5bf2b49de6c06e8c09a275f9ca7693cb9f49b11c
jaozinmafase <jvas2005@outlook.com>
2022-10-18 09:40:04 -0300

    exercicio 8 100%

--- src/services/ingressoCinema.js
+++ src/services/ingressoCinema.js
@@ -1,17 +1,20 @@
-import { getToPathname } from "@remix-run/router";
+export default function calcularIngressos(qtdinteira, qtdmeia, Nacio,diaSemana){
 
-export default function calcularIngressos(qtdinteira, qtdmeia, diaSemana, Nacio){
-let valor = 0;
+let total = `O total a se pagar pelos ingressos é R$ ${(qtdinteira *  28.50) + (qtdmeia * 14.25)}`
 
-if(Nacio === true){ 
-   
+if(qtdinteira < 0 || qtdmeia < 0)
+return 'valores de ingresos inválidos'
+
+if(Nacio){ 
+    total = `O total a se pagar pelos ingressos é R$ ${(qtdinteira * 5) + (qtdmeia * 5) }`
+
 }
 
-if(diaSemana === 'quarta'){
-    Inteira = 14.25
+else if(diaSemana === 'quarta'){
+     total = `O total a se pagar pelos ingressos é R$ ${(qtdinteira * 14.25) + (qtdmeia * 14.25) }`
+
 }
 
 
-return total 
-
+return total
 }
